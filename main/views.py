4776d0e8c94dd71b96fb3ef68889d5bc0c461389
Rehaman <shaik.rehamanrp@gmail.com>
2024-03-27 15:06:09 +0530

    S4 39 templates

--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_view(request):
-    return render(request, '')
+    return render(request, 'views/main.html', {"name" : 'AutoMax'})
