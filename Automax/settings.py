279c4541934720336c851b353944e97da3ebd8e0
Rehaman <shaik.rehamanrp@gmail.com>
2024-04-05 08:49:56 +0530

    extended login functionality and home page afterlogin

--- Automax/settings.py
+++ Automax/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_REDIRECT_URL = '/home/'
+LOGIN_URL = '/login/'
 # Application definition
 
 INSTALLED_APPS = [
--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def main_view(request):
     return render(request, 'views/main.html', {"name" : 'AutoMax'})
+
+@login_required
+def home_view(request):
+    print(request.user.username)
+    return render(request, 'views/home.html')
