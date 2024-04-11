2474b1716c58862e728d54fb57178e8a6d07c275
Rehaman <shaik.rehamanrp@gmail.com>
2024-04-11 21:52:46 +0530

    S6 63 UUID field for string as primary key and foreignkey relationship has been added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
+from users.models import Profile
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class Listing(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True,editable=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    seller=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
